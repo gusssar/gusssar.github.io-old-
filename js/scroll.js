4ab64f0f1f0a405054a0510626a96bc726f95fb2
gusssar <dassler47@gmail.com>
2018-09-04 11:45:38 +0300

    intro

---
+++ js/scroll.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("#id-header-nav").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
+
+$(document).ready(function(){
+    $("#id-content-hello-to-intro").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
