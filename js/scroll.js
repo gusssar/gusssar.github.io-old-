243f579e34e11c8c55e3903a547937d34a744f3a
gusssar <dassler47@gmail.com>
2018-09-04 13:37:45 +0300

    border-fin

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,50 @@
 $(document).ready(function(){
-    $("#id-header-nav").on("click","a", function (event) {
+    $("#id-header-nav-hello-1").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
+
+$(document).ready(function(){
+    $("#id-header-nav-intro-1").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
+
+$(document).ready(function(){
+    $("#id-header-nav-skills-1").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
+
+$(document).ready(function(){
+    $("#id-header-nav-works-1").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
+
+$(document).ready(function(){
+    $("#id-header-nav-about-1").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
+
+$(document).ready(function(){
+    $("#id-header-nav-contact-1").on("click","a", function (event) {
         event.preventDefault();
         var id  = $(this).attr('href'),
             top = $(id).offset().top;
