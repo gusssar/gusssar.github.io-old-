b41f4cc67730f3d23a4c31f5f0569aa0d51de9ab
gusssar <dassler47@gmail.com>
2018-09-09 13:14:53 +0300

    first-publ

--- js/skills.js
+++ js/skills.js
@@ -7,20 +7,20 @@ $(function() {
             datasets: [{
                 data: [10, 9, 5, 6, 5, 4],
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.6)',
-                    'rgba(54, 162, 235, 0.6)',
-                    'rgba(255, 206, 86, 0.6)',
-                    'rgba(75, 192, 192, 0.6)',
-                    'rgba(153, 102, 255, 0.6)',
-                    'rgba(75, 12, 192, 0.6)',
+                    'rgba(0, 0, 0, 0.6)',
+                    'rgba(89, 51, 21, 0.6)',
+                    'rgba(153, 119, 84, 0.6)',
+                    'rgba(83, 55, 122, 0.6)',
+                    'rgba(189, 2, 11, 0.6)',
+                    'rgba(28, 100, 28, 0.6)'
                 ],
                 borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
+                    'rgba(0, 0, 0, 1)',
+                    'rgba(89, 51, 21, 1)',
+                    'rgba(153, 119, 84, 1)',
+                    'rgba(83, 55, 122, 1)',
+                    'rgba(189, 2, 11, 1)',
+                    'rgba(28, 100, 28, 1)'
                 ],
                 borderWidth: 0
 
@@ -29,7 +29,6 @@ $(function() {
         options: {
             responsive: true,
             scale:{
-                //Overlay : false,
                 display:false
 
             },
@@ -54,8 +53,6 @@ $(function() {
             animation:{
                 animateScale: true
             }
-
-            //animation.animateRotate:false
         }
     });
 });
